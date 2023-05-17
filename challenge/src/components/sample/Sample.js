c4cb9bd8fe38c5f4633dbbb424527000a85746c6
dtannyc1 <dtannyc1@gmail.com>
2023-05-17 16:18:49 -0400

    fixed fetch bug

--- challenge/src/components/sample/Sample.js
+++ challenge/src/components/sample/Sample.js
@@ -2,17 +2,29 @@ import {useState, useEffect} from 'react';
 // import * as sample from '../../_assets/sample.json';
 
 const Sample = (props) => {
-    useEffect(() => {
-        const fetchFunc = async () => {await fetch('../../_assets/sample.json', {headers : {'Accept' : 'application/json'}})
-        .then(response => response.json())
-        .then(json => console.log(json))
-    
-    };
-        const sample = fetchFunc();
-    }, [])
+    let data = require('../../_assets/sample.json');
+    // useEffect(() => {
+    //     const fetchFunc = async () => {
+    //         try {
+    //         const response = await fetch('../../_assets/sample.json',
+    //             {
+    //                 method: 'GET',
+    //                 headers : {'Accept' : 'application/json'}
+    //             });
+    //         const data = await response.json();
+    //         console.log(data)
+    //         }
+    //         catch {
+    //             console.log("no good")
+    //         }
+    //     };
+    //     fetchFunc();
+    //     // const sample = fetchFunc();
+    // }, [])
+    console.log(data.id)
     return (
         <h2>Sample Component</h2>
     )
 };
 
-export default Sample; 
+export default Sample;
